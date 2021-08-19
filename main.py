f4764edd9c10eac72f25790c536e235b3b990ca8
AmatorAVG <amatoravg@gmail.com>
2021-08-19 17:51:26 +0700

    Add sliders

--- main.py
+++ main.py
@@ -22,82 +22,40 @@ def load_file(path_to_video):
     player2.loadfile(path_to_video)
     player2.pause()
 
-    # metadata = player.metadata or {}
-    # print(metadata.get('track', ''))
+    align_files()
 
-    player.switch_audio = 2
+    player.switch_audio = int(values['-Player1AT-'])
+    player2.switch_audio = int(values['-Player2AT-'])
     player.sub_visibility = True
-    player.sub = 2
-    # player.sub_source = 2
+    player.sub = int(values['-Player1Sub-'])
 
 
 def play_file():
 
-    # p = player.paused
-    # logger.info(p)
-    #
-    # if  player.paused == Flag:
     player.pause()
-
-    # if player2.paused == Flag:
     player2.pause()
+
 
 def align_files():
 
-    # if not player.paused:
-    #     player.pause()
-    #
-    # if not player2.paused:
-    #     player2.pause()
-
-    # logger.info(player.time_pos)
-    # logger.info(player2.time_pos)
-    # logger.info(player2.time_pos-player.time_pos)
-
-    time_pos = player.time_pos
-
+    time_pos = max(0, player.time_pos - 3)
     player2.time_pos = time_pos
     player.time_pos = time_pos
 
-    # logger.info('***********************')
-    #
-    # logger.info(player.time_pos)
-    # logger.info(player2.time_pos)
-    # logger.info(player2.time_pos - player.time_pos)
-    # player.pause()
-    # player2.pause()
 
-    # player.time_pos = 40
-    # player2.time_pos = 51
-
-    # print(player.length)
-    # Get title from metadata
-    # metadata = player.metadata or {}
-    # print(metadata.get('Title', ''))
-
-    # Print the filename
-    # logger.info(player.filename)
-
-    # Seek +5 seconds
-    # player.time_pos += 5
-    #
-    # # Set to fullscreen
-    # player.fullscreen = True
-    #
-    # # Terminate MPlayer
-    # player.quit()
-
-
+sg.theme('Dark Green 5')
 layout = [
-
-    [sg.Text('Path to video:', size=(35, 1), auto_size_text=False, justification='left'),
-     sg.InputText('/home/amator/Download/Replacements.mkv', key='-PATH-', size=(64, 1)), sg.FileBrowse(file_types=(("Video files", "*.mkv"),))],
-
-    [sg.Button('Open', key=f'btnOpen', size=(22, 1))],
-    [sg.Button('Play/Pause', key=f'btnPlay', size=(22, 1))],  # sg.Button('Pause', key=f'btnPause', size=(22, 1))],
-    [sg.Button('Align', key=f'btnAlign', size=(22, 1))],
-
-    # [sg.Output(size=(112, 12), key='-OUTPUT-')],
+    [sg.Text('Path to video:', size=(15, 1), auto_size_text=False, justification='left'),
+     sg.InputText('/home/amator/Download/Replacements.mkv', key='-PATH-', size=(64, 1)),
+     sg.FileBrowse(file_types=(("Video files", "*.mkv"),))],
+    [sg.Button('Open', key=f'btnOpen', size=(22, 1)), sg.Button('Play/Pause', key=f'btnPlay', size=(22, 1)),
+     sg.Button('Align', key=f'btnAlign', size=(22, 1))],
+    [sg.Text('Player 1 Audio track:', size=(35, 1), auto_size_text=False, justification='left'),
+     sg.Slider(range=(0, 6), orientation='h', size=(34, 20), default_value=2, key='-Player1AT-')],
+    [sg.Text('Player 2 Audio track:', size=(35, 1), auto_size_text=False, justification='left'),
+     sg.Slider(range=(0, 6), orientation='h', size=(34, 20), default_value=0, key='-Player2AT-')],
+    [sg.Text('Player 1 Subtitle:', size=(35, 1), auto_size_text=False, justification='left'),
+     sg.Slider(range=(0, 4), orientation='h', size=(34, 20), default_value=2, key='-Player1Sub-')],
 ]
 win = sg.Window('2Channel player', layout, finalize=True)
 
@@ -109,16 +67,14 @@ while True:
     event, values = win.read()
 
     if event == sg.WIN_CLOSED or event == 'Exit':
-       break
+        break
     elif event == 'btnOpen':
         load_file(values['-PATH-'])
     elif event == 'btnPlay':
         try:
-           play_file()
+            play_file()
         except Exception as err:
             logger.info(err)
-    # elif event == 'btnPause':
-    #         play_file(False)
     elif event == 'btnAlign':
         align_files()
     else:
